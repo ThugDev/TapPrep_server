b819648ae42ecdf0795e121cece8fc2326408275
GDORI <r_louis@naver.com>
2025-01-13 01:22:31 +0900

    update: OAuth 통신구조 추가
    테스트 전
    
    Ref: #2

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,5 @@
+import { config } from '../config/config.js';
+import { url } from '../constants/url.js';
 import { AuthRepository } from '../repositories/auth.repository.js';
 
 export class AuthServices {
@@ -25,9 +27,41 @@ export class AuthServices {
 
   async getAccessToken(code) {
     // OAuth 액세스토큰 취득 로직
+    const response = await fetch(url.git.accessToken, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: config.git.client_id,
+        client_secret: config.git.client_secret,
+        code,
+      }),
+    });
+
+    if (!response.ok || !response.access_token) {
+      // 에러 처리
+    }
+
+    const accessToken = response.access_token;
+
+    return accessToken;
   }
 
   async getGitData(accessToken) {
     // 사용자 정보 취득 로직
+    const userData = await fetch(url.git.user, {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!userData.ok) {
+      // 에러 처리
+    }
+
+    return userData;
   }
 }
