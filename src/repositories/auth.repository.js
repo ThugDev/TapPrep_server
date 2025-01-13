200db4900d5c2a99a536260876c467f780be2fae
GDORI <r_louis@naver.com>
2025-01-13 12:34:05 +0900

    update: OAuth 연동 및 DB연동
    Ref: #2

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,17 +1,72 @@
+import pools from '../mysql/createPool.js';
+import { logger } from '../utils/log/logger.js';
+import { SQL_QUERIES } from './queries.js';
+
 export class AuthRepository {
   async createUser(userData) {
-    // 사용자 생성 로직
+    try {
+      const { login: username, name: nickname, avatar_url: profile_image, email } = userData;
+      if (!username || !nickname || !profile_image || !email) {
+        throw new Error('Invalid arguments');
+      }
+
+      // 사용자 생성 로직
+      const [rows] = await pools.USER_DB.query(SQL_QUERIES.auth.CREATE_USER, [
+        username,
+        nickname,
+        profile_image,
+        email,
+      ]);
+
+      return rows;
+    } catch (err) {
+      logger.error(`${userData.login} - Error creating user : ${err}`);
+      throw new Error('Error creating user');
+    }
   }
 
-  async getUserById(userId) {
-    // 사용자 조회 로직
+  async getUserById(username) {
+    try {
+      if (!username) {
+        throw new Error('Invalid arguments');
+      }
+      // 사용자 조회 로직
+      const [rows] = await pools.USER_DB.query(SQL_QUERIES.auth.FIND_USER_BY_ID, [username]);
+      return rows.length > 0 ? rows[0] : null;
+    } catch (err) {
+      logger.error(`${username} - Error finding user : ${err}`);
+      throw new Error('Error finding user');
+    }
   }
 
-  async updateUser(userId, updateData) {
-    // 사용자 업데이트 로직
+  async updateUserNickname(username, nickname) {
+    try {
+      // 사용자 업데이트 로직
+      if (!username || !nickname) {
+        throw new Error('Invalid arguments');
+      }
+      const [rows] = await pools.USER_DB.query(SQL_QUERIES.auth.UPDATE_USER_NICKNAME, [
+        nickname,
+        username,
+      ]);
+      return rows;
+    } catch (err) {
+      logger.error(`${username} - Error updating nickname : ${err}`);
+      throw new Error('Error updating nickname');
+    }
   }
 
-  async deleteUser(userId) {
+  async deleteUser(username) {
     // 사용자 삭제 로직
+    try {
+      if (!username) {
+        throw new Error('Invalid arguments');
+      }
+      const [rows] = await pools.USER_DB.query(SQL_QUERIES.auth.DELETE_USER, [username]);
+      return rows;
+    } catch (err) {
+      logger.error(`${username} - Error deleting user : ${err}`);
+      throw new Error('Error deleting user');
+    }
   }
 }
