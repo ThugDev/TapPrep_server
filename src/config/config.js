9146f5be2952cd7f63490a543c8c95209c63a8c3
GDORI <r_louis@naver.com>
2025-01-13 12:39:42 +0900

    update: db 관련 환경변수 config 추가
    Ref: #7

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,17 @@
-import { HOST, PEPPER, PORT, SALT, SECRET_KEY } from '../constants/env.js';
+import {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  DB1_HOST,
+  DB1_NAME,
+  DB1_PASSWORD,
+  DB1_PORT,
+  DB1_USER,
+  HOST,
+  PEPPER,
+  PORT,
+  SALT,
+  SECRET_KEY,
+} from '../constants/env.js';
 
 export const config = {
   server: {
@@ -9,5 +22,18 @@ export const config = {
     pepper: PEPPER,
     salt: SALT,
     secret_key: SECRET_KEY,
+  },
+  git: {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  },
+  database: {
+    USER_DB: {
+      name: DB1_NAME,
+      user: DB1_USER,
+      password: DB1_PASSWORD,
+      host: DB1_HOST,
+      port: DB1_PORT,
+    },
   },
 };
--- src/constants/env.js
+++ src/constants/env.js
@@ -10,3 +10,14 @@ export const HOST = process.env.HOST;
 export const PEPPER = process.env.PEPPER;
 export const SALT = process.env.SALT;
 export const SECRET_KEY = process.env.SECRET_KEY;
+
+/* ============================ GIT ============================ */
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+/* ============================ D B ============================ */
+export const DB1_NAME = process.env.DB1_NAME;
+export const DB1_USER = process.env.DB1_USER;
+export const DB1_PASSWORD = process.env.DB1_PASSWORD;
+export const DB1_HOST = process.env.DB1_HOST;
+export const DB1_PORT = process.env.DB1_PORT;
