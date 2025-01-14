5c33a5de41cff0b200c85e13e14bbede1be35ff9
GDORI <r_louis@naver.com>
2025-01-14 21:58:18 +0900

    del: "프로필용으로 분류"
    Ref:#2

--- src/repositories/queries.js
+++ src/repositories/queries.js
@@ -1,9 +1,5 @@
 export const SQL_QUERIES = {
   auth: {
-    FIND_USER_BY_ID: `SELECT * FROM users WHERE username = ?`,
-    CREATE_USER: `INSERT INTO users (username, nickname, profile_image, email) VALUES(?, ?, ?, ?)`,
-    UPDATE_USER_NICKNAME: `UPDATE users SET nickname = ? WHERE username = ?`,
-    UPDATE_USER_PROFILE_IMAGE: `UPDATE users SET profile_image = ? WHERE username = ?`,
     DELETE_USER: `DELETE FROM users WHERE username = ?;`,
   },
 };
