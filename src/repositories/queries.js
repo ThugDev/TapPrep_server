d66b230e8b386222ad3bdb99ec4598d42964cb5a
GDORI <r_louis@naver.com>
2025-01-14 22:04:58 +0900

    refactor:"select문 필요한 것만 불러옴"
    Ref:#2

--- src/repositories/queries.js
+++ src/repositories/queries.js
@@ -1,5 +1,7 @@
 export const SQL_QUERIES = {
   auth: {
+    FIND_USER_BY_ID: `SELECT username, nickname, profile_image FROM users WHERE username = ?`,
+    CREATE_USER: `INSERT INTO users (username, nickname, profile_image, email)`,
     DELETE_USER: `DELETE FROM users WHERE username = ?;`,
   },
 };
