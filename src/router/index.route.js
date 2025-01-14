9d820ff8915373e018aa8d42adf6c5a1bf8e44b5
GDORI <r_louis@naver.com>
2025-01-14 22:07:55 +0900

    update: "프로필 기능 추가로 인한 라우트 수정"
    Ref:#20

--- src/router/index.route.js
+++ src/router/index.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRouter from './auth.route.js';
+import profileRouter from './profile.route.js';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
+router.use('/profile', profileRouter);
 
 export default router;
