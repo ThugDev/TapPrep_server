1a419cc52739543ba1dc228b6b3172f73898846a
GDORI <r_louis@naver.com>
2025-01-19 21:05:43 +0900

    fix: 쿼리문 수정
    Ref:#2

--- src/repositories/queries.js
+++ src/repositories/queries.js
@@ -1,7 +1,7 @@
 export const SQL_QUERIES = {
   auth: {
     FIND_USER_BY_ID: `SELECT username, nickname, profile_image FROM users WHERE username = ?`,
-    CREATE_USER: `INSERT INTO users (username, nickname, profile_image, email)`,
+    CREATE_USER: `INSERT INTO users (username, nickname, profile_image, email) VALUES (?, ?, ?, ?)`,
     DELETE_USER: `DELETE FROM users WHERE username = ?;`,
   },
   profile: {
