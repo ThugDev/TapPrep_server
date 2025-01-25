aeb017ee26d64c3dd7a1bcf48615788e2b134ec2
GDORI <r_louis@naver.com>
2025-01-26 00:43:22 +0900

    refactor: 인증부분 파라미터 미들웨어 추가
    Ref: #2, #10

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -1,13 +1,14 @@
 import { AuthController } from '../controllers/auth.controller.js';
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import { paramsValidator } from '../middlewares/params.middleware.js';
 
 const router = express.Router();
 
 const authController = new AuthController();
 router.get('/git/callback', authController.callback);
-router.post('/git/token', authController.oAuthLogin);
-router.post('/token/refresh', authController.refreshToken);
+router.post('/git/token', paramsValidator.auth.oAuthLogin, authController.oAuthLogin);
+router.post('/token/refresh', paramsValidator.auth.refresh, authController.refreshToken);
 router.post('/logout', authMiddleware, authController.logout);
 
 export default router;
