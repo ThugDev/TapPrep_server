fa0aa7e69b848704a6f98b07395d44c6d9e6764e
GDORI <r_louis@naver.com>
2025-01-26 00:45:56 +0900

    refactor: 레디스 set, get 사용 시 로거 추가
    Ref: #14

--- src/utils/redis/redisClient.js
+++ src/utils/redis/redisClient.js
@@ -1,11 +1,24 @@
 import Redis from 'ioredis';
 import { config } from '../../config/config.js';
+import { redisLogger } from '../log/logger.js';
 
 const redisClient = new Redis({
   host: config.redis.host,
   port: config.redis.port,
   password: config.redis.password,
 });
+
+const originSet = redisClient.set;
+redisClient.set = async function (key, value) {
+  redisLogger.info(`REDIS SET - ${key}`);
+  return originSet.call(this, key, value);
+};
+
+const originGet = redisClient.get;
+redisClient.get = async function (key) {
+  redisLogger.info(`REDIS GET - ${key}`);
+  return originGet.call(this, key);
+};
 
 redisClient.on('connect', () => console.log('Connected to Redis'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
