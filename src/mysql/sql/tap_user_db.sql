83cb27145eb0ae4f868f96a0dec55089a82f03e0
GDORI <r_louis@naver.com>
2025-02-26 01:05:28 +0900

    refactor: 레벨 테이블 분리
    Ref: #35

--- src/mysql/sql/tap_user_db.sql
+++ src/mysql/sql/tap_user_db.sql
@@ -4,7 +4,19 @@ CREATE TABLE IF NOT EXISTS users (
     nickname       VARCHAR(60) NOT NULL,
     profile_image  VARCHAR(255) NOT NULL,
     email           VARCHAR(60) NOT NULL,
-    level           INT DEFAULT 1,
     role            enum('admin', 'user') DEFAULT 'user',
     create_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS userExps (
+    userExp_id      INT AUTO_INCREMENT PRIMARY KEY,
+    user_id         INT NOT NULL,
+    exp             INT NOT NULL DEFAULT 0,
+    level           INT NOT NULL DEFAULT 0,
+    last_level_up   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    create_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) 
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+)
