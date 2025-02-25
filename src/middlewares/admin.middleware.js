28eb1d005c3ec05857fa109b8cfb37b8018e4bbb
GDORI <r_louis@naver.com>
2025-02-25 23:48:42 +0900

    feat: 어드민 미들웨어 추가 및 적용
    Ref: #32

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,23 @@
+import CustomErr from '../utils/error/CustomErr.js';
+import { ERR_CODES } from '../utils/error/ERR_CODES.js';
+import { TokenManager } from '../utils/manager/tokenManager.js';
+
+const tokenManager = new TokenManager();
+
+export default async function (req, _, next) {
+  try {
+    const { accessToken } = req.user; // 유저에서 토큰 정보 추출
+    if (!accessToken)
+      // 토큰이 없을 시 에러
+      throw new CustomErr(ERR_CODES.NOT_FOUND, 'Not Found TOKEN');
+
+    const isAdmin = tokenManager.isAdminToken(accessToken);
+    if (!isAdmin)
+      // 어드민 토큰이 아닐 시 에러
+      throw new CustomErr(ERR_CODES.UNAUTHORIZED, 'Unauthorized');
+
+    next(); // 다음 미들웨어로 전달
+  } catch (err) {
+    next(err);
+  }
+}
--- src/router/problem.route.js
+++ src/router/problem.route.js
@@ -2,25 +2,39 @@ import express from 'express';
 import { ProblemController } from '../controllers/problem.controller.js';
 import { paramsValidator } from '../middlewares/params.middleware.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import adminMiddleware from '../middlewares/admin.middleware.js';
 
 const router = express.Router();
 
 const problemController = new ProblemController();
+
+// 문제 등록, 관리자
 router.post(
   '/',
   authMiddleware,
+  adminMiddleware,
   paramsValidator.problems.createProblem,
   problemController.createProblem,
 );
+
+// 문제 리스트 조회
 router.get(
   '/list',
   authMiddleware,
   paramsValidator.problems.getLists,
   problemController.getProblemList,
 );
+
+// 문제 상세 조회
 router.get('/:problemId', authMiddleware, problemController.getProblem);
-router.patch('/', problemController.updateProblem);
-router.delete('/', problemController.deleteProblem);
+
+// 문제 수정, 관리자
+router.patch('/', authMiddleware, adminMiddleware, problemController.updateProblem);
+
+// 문제 삭제, 관리자
+router.delete('/', authMiddleware, adminMiddleware, problemController.deleteProblem);
+
+// 문제 답안 조회
 router.post(
   '/answer',
   authMiddleware,
