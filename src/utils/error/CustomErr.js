3775335eda28f3a5478555ef68eefd1d777f538b
GDORI <r_louis@naver.com>
2025-03-08 21:00:28 +0900

    refactor: 에러 커스텀클래스 세분화 ( 아직 미적용 )
    Ref:#41

--- src/utils/error/CustomErr.js
+++ src/utils/error/CustomErr.js
@@ -4,3 +4,27 @@ export default class CustomErr extends Error {
     this.statusCode = statusCode; // 추가적인 statusCode 속성 설정
   }
 }
+
+export class BadRequestError extends CustomErr {
+  constructor(message = 'Bad Request') {
+    super(400, message);
+  }
+}
+
+export class UnauthorizedError extends CustomErr {
+  constructor(message = 'Unauthorized') {
+    super(401, message);
+  }
+}
+
+export class NotFoundError extends CustomErr {
+  constructor(message = 'Not Found') {
+    super(404, message);
+  }
+}
+
+export class InternalServerError extends CustomErr {
+  constructor(message = 'Internal Server Error') {
+    super(500, message);
+  }
+}
